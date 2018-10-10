96408ef10ab0ed5ea3dc1f1e2f16665f2df98d1d
Aleesha Kanwal <muhammed.moiz@vistajet.com>
2018-10-10 16:21:09 +0500

    sample code

--- app/src/main/java/tethering/android/com/sample/MainActivity.java
+++ app/src/main/java/tethering/android/com/sample/MainActivity.java
@@ -2,6 +2,12 @@ package tethering.android.com.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import tethering.android.com.tetheringlibrary.Tether;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        String json = new Gson().toJson(Tether.getTetheringClientsList(this));
+        Log.d("isTetheringEnabled",String.valueOf(Tether.isTetheringEnabled(this)));
+        Log.d("tetheringDeviceListJson",json);
+        Tether.getTetheringClientsList(this).clear();
+        for (int i = 0; i < Tether.getTetheringClientsList(this).size(); i++) {
+            Log.d("tetheringDeviceList", "ip : " + Tether.getTetheringClientsList(this).get(i).ipAddr + " " + "mac : " + Tether.getTetheringClientsList(this).get(i).hwAddr);
+        }
     }
 }
